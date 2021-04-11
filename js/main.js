d63a35c0ec56edf8f78bf664cda4e2536fd7e95c
Severino Carvalho da Silva Neto <severinocarvalho14@gmail.com>
2021-04-11 10:32:00 -0300

    Update do JS responsivo

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
 // Script JS
+
+function ajusteConhecimentos() {
+    let global = document.querySelector("#col-frames");
+
+    if (window.innerWidth < 992) {
+        global.classList.add("mt-3");
+    } 
+    if (window.innerWidth >= 992) {
+        global.classList.remove("mt-3");
+        onload();
+    }
+}
+
+window.addEventListener("resize", ajusteConhecimentos);
+window.addEventListener("load", ajusteConhecimentos);
+
