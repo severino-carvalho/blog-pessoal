ea258a400d75586ced0f9e9caf19cd34070805a6
oh-netin <severinocarvalho14@gmail.com>
2021-04-13 11:30:47 -0300

    Update no main.js

--- js/main.js
+++ js/main.js
@@ -1,22 +1,64 @@
 // Script JS
 
-const apiURL = "http://127.0.0.1:63912/json/posts.json";
+const apiURL = "http://127.0.0.1:50705/json/posts.json";
 
 $(document).ready(function () {
 
-    function consultaJquery() {
-        $.getJSON(apiURL, (dados) => console.log(dados));
+    function consultaJquery(indiceInicio, quantidade) {
+        $.getJSON(apiURL, (dados) => {
+            carregarResumoPost(dados, indiceInicio, quantidade)
+        });
     }
-    consultaJquery();
+    consultaJquery(0, 5);
 
 
     function carregarResumoPost(posts, indiceInicio, quantidade) {
         posts.map((post) => post.text = post.text.slice(0, 300));
-        post.reverse();
+
+        for (let i = indiceInicio; i < quantidade; i++) {
+            var post = posts[i];
+
+            var mainDiv = $("<div>");
+            mainDiv.addClass("card mb-4");
+
+            var imgElement = $("<img>");
+            imgElement.addClass("card-img-top");
+            imgElement.attr("src", post.image);
+
+            var bodyDiv = $("<div>");
+            bodyDiv.addClass("card-body");
+
+            var bodyHeader = $("<h2>");
+            bodyHeader.addClass("card-title");
+            bodyHeader.text(post.title);
+
+            var bodyParagraph = $("<p>");
+            bodyParagraph.addClass("card-text");
+            bodyParagraph.text(post.text + "...");
+
+            var bodyLink = $("<a>");
+            bodyLink.addClass("btn btn-primary");
+            bodyLink.html("Leia Mais &rarr;");
+            bodyLink.attr("href", "post.html?id=");
+
+            bodyDiv.append(bodyHeader);
+            bodyDiv.append(bodyParagraph);
+            bodyDiv.append(bodyLink);
+
+            var footerDiv = $("<div>");
+            footerDiv.addClass("card-footer text-muted");
+
+            footerDiv.text("Postado em " + post.datapost);
+
+            mainDiv.append(imgElement);
+            mainDiv.append(bodyDiv);
+            mainDiv.append(footerDiv);
+
+            var blogEntries = $("#blogEntries");
+            blogEntries.append(mainDiv);
+        }
+        indiceInicio += quantidade
     }
-
-    carregarResumoPost();
-
 });
 
 
