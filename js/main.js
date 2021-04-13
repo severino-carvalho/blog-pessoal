ad7b28ac2371188883643d1aa32da5c3df450449
oh-netin <severinocarvalho14@gmail.com>
2021-04-13 14:08:33 -0300

    Update no main.js

--- js/main.js
+++ js/main.js
@@ -20,43 +20,43 @@ $(document).ready(function () {
             var post = posts[i];
 
             var mainDiv = $("<div>");
-            mainDiv.addClass("card mb-4");
+            $(mainDiv).addClass("card mb-4");
 
             var imgElement = $("<img>");
-            imgElement.addClass("card-img-top");
-            imgElement.attr("src", post.image);
+            $(imgElement).addClass("card-img-top");
+            $(imgElement).attr("src", post.image);
 
             var bodyDiv = $("<div>");
-            bodyDiv.addClass("card-body");
+            $(bodyDiv).addClass("card-body");
 
             var bodyHeader = $("<h2>");
-            bodyHeader.addClass("card-title");
-            bodyHeader.text(post.title);
+            $(bodyHeader).addClass("card-title");
+            $(bodyHeader).text(post.title);
 
             var bodyParagraph = $("<p>");
-            bodyParagraph.addClass("card-text");
-            bodyParagraph.html(post.text + "...");
+            $(bodyParagraph).addClass("card-text");
+            $(bodyParagraph).html(post.text + "...");
 
             var bodyLink = $("<a>");
-            bodyLink.addClass("btn btn-primary");
-            bodyLink.html("Leia Mais &rarr;");
-            bodyLink.attr("href", "post.html?id=");
+            $(bodyLink).addClass("btn btn-primary");
+            $(bodyLink).html("Leia Mais &rarr;");
+            $(bodyLink).attr("href", "post.html?id=");
 
-            bodyDiv.append(bodyHeader);
-            bodyDiv.append(bodyParagraph);
-            bodyDiv.append(bodyLink);
+            $(bodyDiv).append(bodyHeader);
+            $(bodyDiv).append(bodyParagraph);
+            $(bodyDiv).append(bodyLink);
 
             var footerDiv = $("<div>");
-            footerDiv.addClass("card-footer text-muted");
+            $(footerDiv).addClass("card-footer text-muted");
 
-            footerDiv.text("Postado em " + post.datapost);
+            $(footerDiv).text("Postado em " + post.datapost);
 
-            mainDiv.append(imgElement);
-            mainDiv.append(bodyDiv);
-            mainDiv.append(footerDiv);
+            $(mainDiv).append(imgElement);
+            $(mainDiv).append(bodyDiv);
+            $(mainDiv).append(footerDiv);
 
             var blogEntries = $("#blogEntries");
-            blogEntries.append(mainDiv);
+            $(blogEntries).append(mainDiv);
         }
         indiceInicio += quantidade
     }
