9f5592518aac8aa03184f171f3daabb181cdd497
oh-netin <severinocarvalho14@gmail.com>
2021-04-13 12:31:14 -0300

    Update no main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // Script JS
 
-//const apiURL = "https://oh-netin.github.io/blog-pessoal/json/posts.json";
-const apiURL = "http://127.0.0.1:53159/json/posts.json";
+const apiURL = "https://oh-netin.github.io/blog-pessoal/json/posts.json";
+//const apiURL = "http://127.0.0.1:53159/json/posts.json";
 
 $(document).ready(function () {
 
