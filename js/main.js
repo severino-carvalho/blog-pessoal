37024384acc9c4ec2cd73901af59f8a576a79d26
oh-netin <severinocarvalho14@gmail.com>
2021-04-12 22:39:42 -0300

    Update no projeto

--- js/main.js
+++ js/main.js
@@ -1,2 +1,27 @@
 // Script JS
 
+$(document).ready(function () {
+    $.ajax({
+        url: "http://127.0.0.1:63912/json/posts.json",
+        type: "GET"
+    })
+        .done(function (dados) {
+            console.log(dados);
+            alert(dados[0].title)
+        });
+});
+
+function carregarPost(objPost, indexInicio, quantidade) {
+    for (let i = indexInicio; i < quantidade; i++) {
+        var post = objPost[i];
+
+        var mainDiv = $("<div>");
+        mainDiv.addClass("card mb-4"); 
+
+        var imgElement = $("<img>");
+        imgElement.addClass("card-img-top");
+        imgElement.attr("src", "") 
+    }
+}
+
+carregarPost();
