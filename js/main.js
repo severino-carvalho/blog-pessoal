3fde9abaf30b431f187299f61f4525139bdc3d9d
oh-netin <severinocarvalho14@gmail.com>
2021-04-12 23:07:44 -0300

    Att main.js

--- js/main.js
+++ js/main.js
@@ -1,27 +1,22 @@
 // Script JS
 
+const apiURL = "http://127.0.0.1:63912/json/posts.json";
+
 $(document).ready(function () {
-    $.ajax({
-        url: "http://127.0.0.1:63912/json/posts.json",
-        type: "GET"
-    })
-        .done(function (dados) {
-            console.log(dados);
-            alert(dados[0].title)
-        });
+
+    function consultaJquery() {
+        $.getJSON(apiURL, (dados) => console.log(dados));
+    }
+    consultaJquery();
+
+
+    function carregarResumoPost(posts, indiceInicio, quantidade) {
+        posts.map((post) => post.text = post.text.slice(0, 300));
+        post.reverse();
+    }
+
+    carregarResumoPost();
+
 });
 
-function carregarPost(objPost, indexInicio, quantidade) {
-    for (let i = indexInicio; i < quantidade; i++) {
-        var post = objPost[i];
 
-        var mainDiv = $("<div>");
-        mainDiv.addClass("card mb-4"); 
-
-        var imgElement = $("<img>");
-        imgElement.addClass("card-img-top");
-        imgElement.attr("src", "") 
-    }
-}
-
-carregarPost();
