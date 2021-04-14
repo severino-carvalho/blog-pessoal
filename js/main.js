bfc0a53daa760f637cb2ed3f02b6924030b9a1bb
oh-netin <severinocarvalho14@gmail.com>
2021-04-14 20:48:19 -0300

    add link dinâmicamente

--- js/main.js
+++ js/main.js
@@ -3,61 +3,86 @@
 const apiURL = "https://oh-netin.github.io/blog-pessoal/json/posts.json";
 //const apiURL = "http://127.0.0.1:55595/json/posts.json"
 
+function carregarResumoPost(posts, indiceInicio, quantidade) {
+    posts.map((post) => post.text = post.text.slice(0, 300));
+
+    for (let i = indiceInicio; i < quantidade; i++) {
+        var post = posts[i];
+
+        var mainDiv = $("<div>");
+        $(mainDiv).addClass("card mb-4");
+
+        var imgElement = $("<img>");
+        $(imgElement).addClass("card-img-top");
+        $(imgElement).attr("src", post.image);
+
+        var bodyDiv = $("<div>");
+        $(bodyDiv).addClass("card-body");
+
+        var bodyHeader = $("<h2>");
+        $(bodyHeader).addClass("card-title");
+        $(bodyHeader).text(post.title);
+
+        var bodyParagraph = $("<p>");
+        $(bodyParagraph).addClass("card-text");
+        $(bodyParagraph).html(post.text + "...");
+
+        var bodyLink = $("<a>");
+        $(bodyLink).addClass("btn btn-primary");
+        $(bodyLink).html("Leia Mais &rarr;");
+        $(bodyLink).attr("href", "html/posts.html?id=" + post.id);
+
+        $(bodyDiv).append(bodyHeader);
+        $(bodyDiv).append(bodyParagraph);
+        $(bodyDiv).append(bodyLink);
+
+        var footerDiv = $("<div>");
+        $(footerDiv).addClass("card-footer text-muted");
+
+        $(footerDiv).text("Postado em " + post.datapost);
+
+        $(mainDiv).append(imgElement);
+        $(mainDiv).append(bodyDiv);
+        $(mainDiv).append(footerDiv);
+
+        var blogEntries = $("#blogEntries");
+        $(blogEntries).append(mainDiv);
+    }
+    indiceInicio += quantidade
+}
+
+function consultaJquery(indiceInicio, quantidade) {
+    $.getJSON(apiURL, (dados) => {
+        carregarResumoPost(dados, indiceInicio, quantidade)
+    });
+}
+
+function requisicaoLink() {
+    $.getJSON(apiURL, (dados) => {
+        carregarLinks(dados);
+    });
+}
+
+function carregarLinks(dados) {
+    var tagLinks = $(".addLink").toArray();
+    console.log(tagLinks);
+
+    $(tagLinks).each(function (index, link) {
+        $(link).attr("href", "html/posts.html?id=" + dados[index].id);
+    });
+}
+
 $(document).ready(function () {
 
-    function consultaJquery(indiceInicio, quantidade) {
-        $.getJSON(apiURL, (dados) => {
-            carregarResumoPost(dados, indiceInicio, quantidade)
-        });
-    }
+    $("#header-categoria").click(function () {
+        $("#body-categoria").slideToggle(700);
+    });
+
+    $("#header-autor").click(function () {
+        $("#body-autor").slideToggle(1000);
+    });
+
+    requisicaoLink();
+
     consultaJquery(0, 5);
-
-
-    function carregarResumoPost(posts, indiceInicio, quantidade) {
-        posts.map((post) => post.text = post.text.slice(0, 300));
-
-        for (let i = indiceInicio; i < quantidade; i++) {
-            var post = posts[i];
-
-            var mainDiv = $("<div>");
-            $(mainDiv).addClass("card mb-4");
-
-            var imgElement = $("<img>");
-            $(imgElement).addClass("card-img-top");
-            $(imgElement).attr("src", post.image);
-
-            var bodyDiv = $("<div>");
-            $(bodyDiv).addClass("card-body");
-
-            var bodyHeader = $("<h2>");
-            $(bodyHeader).addClass("card-title");
-            $(bodyHeader).text(post.title);
-
-            var bodyParagraph = $("<p>");
-            $(bodyParagraph).addClass("card-text");
-            $(bodyParagraph).html(post.text + "...");
-
-            var bodyLink = $("<a>");
-            $(bodyLink).addClass("btn btn-primary");
-            $(bodyLink).html("Leia Mais &rarr;");
-            $(bodyLink).attr("href", "html/posts.html?id=" +post.id);
-
-            $(bodyDiv).append(bodyHeader);
-            $(bodyDiv).append(bodyParagraph);
-            $(bodyDiv).append(bodyLink);
-
-            var footerDiv = $("<div>");
-            $(footerDiv).addClass("card-footer text-muted");
-
-            $(footerDiv).text("Postado em " + post.datapost);
-
-            $(mainDiv).append(imgElement);
-            $(mainDiv).append(bodyDiv);
-            $(mainDiv).append(footerDiv);
-
-            var blogEntries = $("#blogEntries");
-            $(blogEntries).append(mainDiv);
-        }
-        indiceInicio += quantidade
-    }
 });
--- js/posts.js
+++ js/posts.js
@@ -6,7 +6,7 @@ const postID = new URLSearchParams(window.location.search).get("id");
 function requisicaoPost() {
     $.getJSON(apiURL, (dados) => {
         if (Number(postID) > dados.length) {
-           alert("Seu corno hacker!")
+            alert("Seu corno hacker!")
         } else {
             carregarPost(dados);
         }
@@ -15,10 +15,10 @@ function requisicaoPost() {
 
 function carregarPost(dados) {
     let id = Number(dados[postID - 1].id) - 1;
-    
+
     document.title = "Conteúdo - " + dados[id].title;
 
-    $("#postAtivo").text(dados[id].title)
+    $("#postAtivo").text(dados[id].title);
 
     $("#postTitulo").text(dados[id].title);
 
@@ -31,6 +31,35 @@ function carregarPost(dados) {
     $("#postConteudo").append("<p>" + dados[id].text + "</p>");
 }
 
+function requisicaoLink() {
+    $.getJSON(apiURL, (dados) => {
+        if (Number(postID) > dados.length) {
+            alert("Seu corno hacker!")
+        } else {
+            carregarLinks(dados);
+        }
+    });
+}
+
+function carregarLinks(dados) {
+    var tagLinks = $(".addLink").toArray();
+    console.log(tagLinks);
+
+    $(tagLinks).each(function (index, link) {
+        $(link).attr("href", "posts.html?id=" + dados[index].id);
+    });
+}
+
 $(document).ready(function () {
+    $("#header-categoria").click(function () {
+        $("#body-categoria").slideToggle(700);
+    });
+
+    $("#header-autor").click(function () {
+        $("#body-autor").slideToggle(1000);
+    });
+
+    requisicaoLink();
+
     requisicaoPost();
 });
