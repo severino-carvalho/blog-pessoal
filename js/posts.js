35c067121542d6a1affdb3cbf76c8708454b3684
oh-netin <severinocarvalho14@gmail.com>
2021-04-14 15:47:15 -0300

    Update no projeto

--- js/posts.js
+++ js/posts.js
@@ -5,14 +5,17 @@ const postID = new URLSearchParams(window.location.search).get("id");
 
 function requisicaoPost() {
     $.getJSON(apiURL, (dados) => {
-        carregarPost(dados);
+        if (Number(postID) > dados.length) {
+           alert("Seu corno hacker!")
+        } else {
+            carregarPost(dados);
+        }
     });
 }
 
 function carregarPost(dados) {
     let id = Number(dados[postID - 1].id) - 1;
-    console.log(id);
-
+    
     document.title = "Conteúdo - " + dados[id].title;
 
     $("#postAtivo").text(dados[id].title)
